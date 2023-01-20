2ae5fe3c07b89b1e2a09f74be2a518295ff940b7
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-01-20 20:27:23 +0530

    Update string_copy.c

--- Advanced-C/String/string_copy.c
+++ Advanced-C/String/string_copy.c
@@ -1,1 +1,16 @@
+#include<stdio.h>
 
+int main()
+{
+	char str[10], copy_str[10];
+	scanf("%s", str);
+    int i = 0;
+	while(str[i] != '\0')
+	{
+	   copy_str[i] = str[i];
+	   i++;
+	}
+	
+	printf("Copied string = %s",copy_str);
+	return 0;
+}
