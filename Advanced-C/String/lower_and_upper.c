a012e9dcd965856cece1ffd5ec5f021377718492
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-01-20 21:35:20 +0530

    Update lower_and_upper.c

--- Advanced-C/String/lower_and_upper.c
+++ Advanced-C/String/lower_and_upper.c
@@ -1,1 +1,18 @@
 
+#include<stdio.h>
+
+int main()
+{
+	char str[10];
+	scanf("%s", str);
+
+	for(int i = 0; str[i] != '\0'; i++)
+	{
+	    if(str[i] >= 'A' && str[i] <= 'Z')
+	    str[i] = str[i] + 32;
+	    else if (str[i] >= 'a' && str[i] <= 'z')
+	    str[i] = str[i] - 32;
+	}
+	printf("%s",str);
+	return 0;
+} 
