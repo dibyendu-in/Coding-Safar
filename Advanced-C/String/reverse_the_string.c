0a07a5cb2b460af5670ed67908095290b1c86195
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-01-20 21:51:21 +0530

    Create reverse_the_string.c

---
+++ Advanced-C/String/reverse_the_string.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+	char str[10];
+	scanf("%s", str);
+    int i;
+	for(i = 0; str[i] != '\0'; i++)
+	{
+	    printf("%c", str[strlen(str)-i-1]);
+	}
+
+	return 0;
+}
