aa457728faa606e9c2ca2ce9e3afffb998a882c3
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-01-21 08:37:54 +0530

    Update palindrome_of_the_string.c

--- Advanced-C/String/palindrome_of_the_string.c
+++ Advanced-C/String/palindrome_of_the_string.c
@@ -1,1 +1,31 @@
+#include<stdio.h>
 
+int strlength(char *str)
+{
+    int count = 0;
+    for(int i = 0; str[i] != '\0'; i++)
+    count++;
+    return count;
+}
+
+int main()
+{
+	char str[10];
+	scanf("%s", str);
+    int i, flag = 0, len;
+    
+    len = strlength(str);
+    
+	for(i = 0; i < len/2; i++)
+	{
+	   if(str[i] != str[len-i-1])
+	    flag = 1;
+	    break;
+	}
+	if(flag == 0)
+	printf("Palindrome\n");
+	else
+	printf("Not Palindrome\n");
+
+	return 0;
+}
