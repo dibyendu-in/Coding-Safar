b270eafd24d386eca8667f1a999475d4ad2c37e4
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-01-22 19:36:54 +0530

    Create Swap-two-numbers-using-bitwise.c

---
+++ Advanced-C/Bitwise/Swap-two-numbers-using-bitwise.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main()
+{
+	int a, b, c, d;
+
+	scanf("%d%d", &a, &b);
+    c = a;
+    d = b;
+    
+	a = (c ^ d) ^ c;
+	b = (c ^ d) ^ d;
+
+	printf("%d %d", a, b);
+	
+	return 0;
+}
