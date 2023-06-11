ac7b6359652d2490a00edfbf2e3e92f7e36e9cad
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-11 10:36:03 +0530

    Create main.h

---
+++ Micro-Controllers/03_Project_LED_Pattern/main.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.h
+ * Author: DIBYENDU BARMAN
+ *
+ * Created on June 10, 2023, 9:27 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED_ARRAY     PORTB
+#define LED_ARRAY_DDR TRISB
+
+#endif	/* MAIN_H */
