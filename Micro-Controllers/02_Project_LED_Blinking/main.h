800bc2a1f50e0b0b0e51aadc8e8fc201f5b5681d
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-11 09:11:53 +0530

    Create main.h

---
+++ Micro-Controllers/02_Project_LED_Blinking/main.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   main.h
+ * Author: DIBYENDU BARMAN
+ *
+ * Created on June 10, 2023, 9:27 PM
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED_ARRAY     PORTD
+#define LED_ARRAY_DDR TRISD
+
+#endif	/* MAIN_H */
