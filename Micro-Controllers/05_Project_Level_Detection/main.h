9abc06d704610ee74e7863e80aebb05a96f6af59
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-11 13:42:25 +0530

    Create main.h

---
+++ Micro-Controllers/05_Project_Level_Detection/main.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   main.h
+ */
+
+#ifndef MAIN_H
+#define	MAIN_H
+
+#define LED_ARRAY2          PORTD
+#define LED_ARRAY2_DDR      TRISD
+
+#define LED1_DDR            TRISD0
+#define SW1_DDR             TRISB0
+
+#define LED1                RD0
+#define SW1                 RB0
+
+#define ON                  1
+#define OFF                 0
+
+#define PRESSED             0
+
+#endif	/* MAIN_H */
