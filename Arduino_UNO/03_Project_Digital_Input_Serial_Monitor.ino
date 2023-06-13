e666d7dbcde41f0250d66b5af6fc08ee19bd71eb
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-13 18:19:43 +0530

    Create 03_Project_Digital_Input_Serial_Monitor.ino

---
+++ Arduino_UNO/03_Project_Digital_Input_Serial_Monitor.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(12, INPUT_PULLUP);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int val = digitalRead(12);
+  Serial.println(val);
+}
