04e1ca66d063a7c99fb1770cc45659118975b051
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-06-14 16:26:59 +0530

    Update 09_Project_for_loop_Control_LED.ino

--- Arduino_UNO/09_Project_for_loop_Control_LED.ino
+++ Arduino_UNO/09_Project_for_loop_Control_LED.ino
@@ -13,12 +13,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   for(int i = 0; i <= 255; i++)
   {
-    analogWrite(10, i);
-    delay(10);
+    analogWrite(11, i);
+    delay(15);
   }
   for(int j = 255; j >= 0; j--)
   {
-    analogWrite(10, j);
-    delay(10);
+    analogWrite(11, j);
+    delay(15);
   }
 }
