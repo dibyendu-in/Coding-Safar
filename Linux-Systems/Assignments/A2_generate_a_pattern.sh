17843536d949dbd7ee1515550ed29ae2fb4ed62b
Dibyendu Barman <119417662+DibyenduBarmanOfficial@users.noreply.github.com>
2023-10-13 09:12:32 +0530

    Create A2_generate_a_pattern.sh

---
+++ Linux-Systems/Assignments/A2_generate_a_pattern.sh
@@ -0,0 +1,28 @@
+<< Documentation
+Name: Dibyendu Barman
+Date:15/Sept/2022
+Description: Write a program read 'n' and generate a pattern given below
+( Number increasing from top to bottom. )
+Sample Input: 5
+Sample Output:
+1
+2 3
+4 5 6
+7 8 9 10
+11 12 13 14 15
+Documentation
+
+#!/bin/bash
+
+read -p "Enter a number: " n  # read the input number from user
+count=1
+for i in `seq $n`          # 'i' is the number of row
+do
+        for j in `seq $i`  # 'j' is the the number of column
+        do
+                echo -n "$count " # '-n' used to print output in same line
+
+                count=$(($count+1))  # each iteration it increasing the value of 'count' variable
+        done
+        echo    # 'echo' used for print a new line
+done
